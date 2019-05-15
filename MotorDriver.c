7ece4562ccc91ce360864802bc15ae6c1856a843
AuHermann <48994899+AuHermann@users.noreply.github.com>
2019-05-15 22:07:26 +0200

    Motordriver

---
+++ MotorDriver.c
@@ -0,0 +1,45 @@
+/*
+ * MotorDriver.c
+ *
+ * Created: 15-05-2019 08:32:15
+ *  Author: Andreas Hermann
+ */ 
+#include "MotorDriver.h"
+#include <avr/io.h>
+
+void initMotor()
+{
+	DDRL = 0xff;
+	TCCR5A = 0b10000011;
+	TCCR5B = 0b00000000;
+	setSpeed(512);
+}
+
+void start()
+{
+	TCCR5B |= 0b00000101;
+}
+
+void stop()
+{
+	TCCR5B &= 0b00000000;
+}
+
+void setSpeed(int speed)
+{
+	OCR5A = (speed*(1024/100));
+}
+
+void setDirection(unsigned char dir)
+{
+	if(dir == 0)
+	{
+		PORTL &= 0b01111111;
+		PORTL |= 0b01000000; 
+	}
+	else if( dir == 1)
+		{
+		PORTL |= 0b10000000;
+		PORTL &= 0b10111111;
+		}
+}
---
+++ MotorDriver.h
@@ -0,0 +1,17 @@
+/*
+ * MotorDriver.h
+ *
+ * Created: 15-05-2019 08:32:47
+ *  Author: Andreas Hermann
+ */ 
+
+
+
+void initMotor();
+void start();
+void stop();
+void setSpeed(int speed);
+void setDirection(unsigned char dir);
+
+
+
