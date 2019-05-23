06e2575619e4fdf6fd32a9bf031764b0d27728c3
bas10an <50912336+bas10an@users.noreply.github.com>
2019-05-23 14:55:01 +0200

    Add files via upload

--- BilKode/satsammen2/MP3_Driver.c
+++ BilKode/satsammen2/MP3_Driver.c
@@ -2,37 +2,15 @@
  * MP3_Driver.c
  *
  * Created: 15-05-2019 09:48:28
- *  Author: dryos
+ *  Author: Bastian
  */ 
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
 #include "uart.h"
 
 void initMP3()
 {
-	SendChar(0x7E);
-	SendChar(0x0C);
-	SendChar(0x00);
-	SendChar(0x00);
-	SendChar(0x00);
-	SendChar(0xFF);
-	SendChar(0xF4);
-	SendChar(0xEF);
-}
-void playSound(char soundChoice)
-{
-	unsigned char x = (0xFC - soundChoice) + 1;
-	SendChar(0x7E);
-	SendChar(0x03);
-	SendChar(0x00);
-	SendChar(0x00);
-	SendChar(soundChoice);
-	SendChar(0xFF);
-	SendChar(x);			//er forskellig ved forskellige tracks... what do
-	SendChar(0xEF);			// 1. Tema	2. DOC	3.IT Works
-}
-void volumeSet()
-{
-	//Volume set MAX
 	SendChar(0x7E);
 	SendChar(0x06);
 	SendChar(0x00);
@@ -40,5 +18,23 @@ void volumeSet()
 	SendChar(0x1E);
 	SendChar(0xFF);
 	SendChar(0xDC);
+	SendChar(0xEF);
+	_delay_ms(1000);
+}
+void playSound(char soundChoice)
+{
+	// udregning af 16bit checksum, der senere deles op i to
+	//  0xFFFF - (CMD + feedback + para1 + para2) + 1
+	//	Vi bruger ikke sange nok til at de 8 MSB ændres. Derfor bruges blot 0xFF for de 8 LSB
+	
+	unsigned char x = 0xFF - (0x03 + 0x00 + 0x00 + soundChoice) + 1;
+	
+	SendChar(0x7E);
+	SendChar(0x03);			//CMD
+	SendChar(0x00);			//FeedBack
+	SendChar(0x00);			//para1
+	SendChar(soundChoice);	//para2
+	SendChar(0xFF);
+	SendChar(x);			//8 LSB af Checksum (x)
 	SendChar(0xEF);
 }
